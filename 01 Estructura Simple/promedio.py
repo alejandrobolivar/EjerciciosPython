719146547196a6c10f0719c3d7bd2f9f372568f9
alejandrobolivar <bolivara@gmail.com>
2021-02-25 19:27:14 -0400

    subido al repo

---
+++ 01 Estructura Simple/promedio.py
@@ -0,0 +1,11 @@
+#Primero, pedimos las 4 notas
+nota_1 = int(input("Primer valor: "))
+nota_2 = int(input("Segundo valor: "))
+nota_3 = int(input("Tercer valor: "))
+
+#Calculamos el promedio y lo mostramos en pantalla. Nota el decimal en el 4.0, de esa forma la division entregara numeros decimales.
+promedio = (nota_1+nota_2+nota_3)/3.0
+print "El promedio es: "+str(promedio)
+
+# Fin del programa
+input("Pulse una tecla para finalizar...") #'Pausa
